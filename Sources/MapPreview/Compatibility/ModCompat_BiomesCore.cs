4f540c5c2173f96e60ad8eea7c1c3d1637f53f49
m00nl1ght <github@m00nl1ght.dev>
2023-04-21 01:01:49 +0200

    Make Biomes! Core compatibility patch optional

--- Sources/MapPreview/Compatibility/ModCompat_BiomesCore.cs
+++ Sources/MapPreview/Compatibility/ModCompat_BiomesCore.cs
@@ -9,6 +9,11 @@ internal class ModCompat_BiomesCore : ModCompat
     public override string TargetAssemblyName => "BiomesCore";
     public override string DisplayName => "Biomes Core";
 
+    protected override bool OnApply()
+    {
+        return AccessTools.Method("BiomesCore.Patches.WildPlantSpawner_GetBaseDesiredPlantsCountAt:UpdateCommonalityAt") != null;
+    }
+
     [HarmonyPrefix]
     [HarmonyPatch("BiomesCore.Patches.WildPlantSpawner_GetBaseDesiredPlantsCountAt", "UpdateCommonalityAt")]
     private static bool WildPlantSpawner_GetBaseDesiredPlantsCountAt_UpdateCommonalityAt()
