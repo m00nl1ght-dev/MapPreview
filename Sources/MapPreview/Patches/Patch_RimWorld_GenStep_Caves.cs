b74e7ffd8ad95ef8d112a000fe0995dac6166410
m00nl1ght <github@m00nl1ght.dev>
2024-04-12 21:12:04 +0200

    Fix cave generator patch conflicting with Anomaly content

--- Sources/MapPreview/Patches/Patch_RimWorld_GenStep_Caves.cs
+++ Sources/MapPreview/Patches/Patch_RimWorld_GenStep_Caves.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using LunarFramework.Patching;
 using RimWorld;
@@ -17,32 +18,32 @@ internal static class Patch_RimWorld_GenStep_Caves
     [HarmonyTranspiler]
     [HarmonyPatch("Dig")]
     [PatchExcludedFromConflictCheck]
-    private static IEnumerable<CodeInstruction> Dig_Transpiler(IEnumerable<CodeInstruction> instructions)
-        => RemoveRedundantRehash(instructions, "tmpGroupSet");
+    private static IEnumerable<CodeInstruction> Dig_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        => RemoveRedundantRehash(instructions, generator, "tmpGroupSet");
 
     [HarmonyTranspiler]
     [HarmonyPatch("GetDistToCave")]
     [PatchExcludedFromConflictCheck]
-    private static IEnumerable<CodeInstruction> GetDistToCave_Transpiler(IEnumerable<CodeInstruction> instructions)
-        => RemoveRedundantRehash(instructions, "tmpGroupSet");
+    private static IEnumerable<CodeInstruction> GetDistToCave_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        => RemoveRedundantRehash(instructions, generator, "tmpGroupSet");
 
     [HarmonyTranspiler]
     [HarmonyPatch("FindRandomEdgeCellForTunnel")]
     [PatchExcludedFromConflictCheck]
-    private static IEnumerable<CodeInstruction> FindRandomEdgeCellForTunnel_Transpiler(IEnumerable<CodeInstruction> instructions)
-        => RemoveRedundantRehash(instructions, "tmpGroupSet");
+    private static IEnumerable<CodeInstruction> FindRandomEdgeCellForTunnel_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        => RemoveRedundantRehash(instructions, generator, "tmpGroupSet");
 
     [HarmonyTranspiler]
     [HarmonyPatch("GetDistToNonRock", typeof(IntVec3), typeof(List<IntVec3>), typeof(IntVec3), typeof(int))]
     [PatchExcludedFromConflictCheck]
-    private static IEnumerable<CodeInstruction> GetDistToNonRock_ByOffset_Transpiler(IEnumerable<CodeInstruction> instructions)
-        => RemoveRedundantRehash(instructions, "groupSet");
+    private static IEnumerable<CodeInstruction> GetDistToNonRock_ByOffset_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        => RemoveRedundantRehash(instructions, generator, "groupSet");
 
     [HarmonyTranspiler]
     [HarmonyPatch("GetDistToNonRock", typeof(IntVec3), typeof(List<IntVec3>), typeof(float), typeof(int))]
     [PatchExcludedFromConflictCheck]
-    private static IEnumerable<CodeInstruction> GetDistToNonRock_ByDir_Transpiler(IEnumerable<CodeInstruction> instructions)
-        => RemoveRedundantRehash(instructions, "groupSet");
+    private static IEnumerable<CodeInstruction> GetDistToNonRock_ByDir_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        => RemoveRedundantRehash(instructions, generator, "groupSet");
 
     [HarmonyPostfix]
     [HarmonyPatch("RemoveSmallDisconnectedSubGroups")]
@@ -51,14 +52,26 @@ internal static class Patch_RimWorld_GenStep_Caves
         ___tmpGroupSet = ___groupSet;
     }
 
-    private static IEnumerable<CodeInstruction> RemoveRedundantRehash(IEnumerable<CodeInstruction> instructions, string fieldName)
+    private static IEnumerable<CodeInstruction> RemoveRedundantRehash(IEnumerable<CodeInstruction> instructions, ILGenerator generator, string fieldName)
     {
+        var labelAfter = generator.DefineLabel();
+        var ldargGroup = new CodeInstruction(OpCodes.Ldarg);
+        var ldfldStatic = CodeInstruction.LoadField(typeof(GenStep_Caves), fieldName);
+
         var pattern = TranspilerPattern.Build("RemoveRedundantRehash")
-            .MatchLoad(typeof(GenStep_Caves), fieldName).Nop()
-            .MatchCall(typeof(HashSet<IntVec3>), "Clear").Nop()
-            .MatchLoad(typeof(GenStep_Caves), fieldName).Nop()
-            .MatchLdarg().Nop()
-            .Match(ci => ci.operand is MethodInfo { Name: "AddRange" }).Nop()
+            .MatchLoad(typeof(GenStep_Caves), fieldName).Nop() // to preserve labels
+            .Insert(ldfldStatic)
+            .Insert(CodeInstruction.Call(typeof(HashSet<IntVec3>), "get_Count"))
+            .Insert(ldargGroup)
+            .Insert(CodeInstruction.Call(typeof(List<IntVec3>), "get_Count"))
+            .Insert(OpCodes.Ceq)
+            .Insert(new CodeInstruction(OpCodes.Brtrue_S, labelAfter))
+            .Insert(CodeInstruction.LoadField(typeof(GenStep_Caves), fieldName))
+            .MatchCall(typeof(HashSet<IntVec3>), "Clear").Keep()
+            .MatchLoad(typeof(GenStep_Caves), fieldName).Keep()
+            .MatchLdarg().StoreIn(ldargGroup).Keep()
+            .Match(ci => ci.operand is MethodInfo { Name: "AddRange" }).Keep()
+            .MatchAny().Do(ci => ci.labels.Add(labelAfter))
             .Greedy();
 
         return TranspilerPattern.Apply(instructions, pattern);
