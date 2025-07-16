41110d8d70e431a5fd1c90160f834effc36806d1
m00nl1ght <github@m00nl1ght.dev>
2025-07-16 23:00:50 +0200

    Update ModCompat for Vanilla Expanded Framework

--- Sources/MapPreview/Compatibility/ModCompat_VE_VEF.cs
+++ Sources/MapPreview/Compatibility/ModCompat_VE_VEF.cs
@@ -4,13 +4,22 @@ using LunarFramework.Patching;
 namespace MapPreview.Compatibility;
 
 [HarmonyPatch]
-internal class ModCompat_VE_VFECore : ModCompat
+internal class ModCompat_VE_VEF : ModCompat
 {
+    #if RW_1_6_OR_GREATER
+    public override string TargetAssemblyName => "VEF";
+    #else
     public override string TargetAssemblyName => "VFECore";
+    #endif
+
     public override string DisplayName => "Vanilla Expanded Framework";
 
     [HarmonyPrefix]
+    #if RW_1_6_OR_GREATER
+    [HarmonyPatch("VEF.Maps.VanillaExpandedFramework_TerrainGrid_SetTerrain", "Postfix")]
+    #else
     [HarmonyPatch("VFECore._TerrainGrid", "Postfix")]
+    #endif
     private static bool TerrainGrid_SetTerrain_Postfix()
     {
         return !MapPreviewGenerator.IsGeneratingOnCurrentThread;
